69912de10586be19d113212a6ec9f48b054f2ff2
sukhajata <sukhajata@gmail.com>
2019-01-07 16:20:30 +0700

    Added api key

--- index.js
+++ index.js
@@ -68,5 +68,6 @@ async function getDirections(res, origin, destination, waypoints=[]) {
         return { 'error': err };
     }
 }
-const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
